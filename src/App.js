4ad46ecccf319574166da346f6b333976f3945c6
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-11-03 16:08:55 -0300

    Requisito: 01 (criando paginas)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Link to="/pages/" component={ Login } />
+        <Link to="/pages/search" component={ Search } />
+        <Link to="/pages/album/:id" component={ Album } />
+        <Link to="/pages/favorites" component={ Favorites } />
+        <Link to="/src/pages/profile" component={ Profile } />
+        <Link to="/src/pages/profile/edit" component={ ProfileEdit } />
+        <Link to="/src/pages/NotFound" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import { Route, BrowserRouter } from 'react-router-dom';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div data-testid="page-login" />
+    );
+  }
+}
