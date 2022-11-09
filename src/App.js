431c2274b082287e5beebe140bbc5d7a52a469ca
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-11-09 03:52:34 -0300

    Evoluindo requisito 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,18 +7,67 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.isButtonDisabled = this.isButtonDisabled.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+  }
+
+  state = {
+    loginInput: '',
+  };
+
+  onInputChange = (event) => {
+    const { name, type, checked } = event.target;
+    const value = type === 'checkbox' ? checked : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  isButtonDisabled = () => {
+    const { loginInput } = this.state;
+    const number = 3;
+    const validate = loginInput.length >= number;
+
+    return !(validate);
+  };
+
+  onSaveButtonClick = (event) => {
+    event.preventDefault();
+    const { loginInput } = this.state;
+
+    createUser({ name: loginInput });
+
+    this.setState({
+      loginInput: '',
+    });
+  };
+
   render() {
+    const { loginInput } = this.state;
     return (
       <BrowserRouter>
-        <Link to="/" component={ Login } />
-        <Link to="/search" component={ Search } />
-        <Link to="/album/:id" component={ Album } />
-        <Link to="/favorites" component={ Favorites } />
-        <Link to="/profile" component={ Profile } />
-        <Link to="/profile/edit" component={ ProfileEdit } />
-        <Link to="/NotFound" component={ NotFound } />
+        <Switch>
+          <Link
+            to="/"
+            component={ Login }
+            loginInput={ loginInput }
+            onInputChange={ this.onInputChange }
+            isButtonDisabled={ this.isButtonDisabled() }
+            onSaveButtonClick={ this.onSaveButtonClick }
+          />
+          <Link to="/search" component={ Search } />
+          <Link to="/album/:id" component={ Album } />
+          <Link to="/favorites" component={ Favorites } />
+          <Link to="/profile" component={ Profile } />
+          <Link to="/profile/edit" component={ ProfileEdit } />
+          <Link to="/NotFound" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
-// import { Route, BrowserRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
   render() {
+    const {
+      loginInput,
+      isButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <input
+            type="text"
+            name="loginInput"
+            loginInput={ loginInput }
+            data-testid="login-name-input"
+            value={ loginInput }
+            onChange={ onInputChange }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ isButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  loginInput: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  isButtonDisabled: PropTypes.bool.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
