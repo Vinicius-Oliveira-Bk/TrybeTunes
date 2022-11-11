9d999f10908a8b3097e0e2139995fcba53fa0fe6
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-11-11 19:33:10 -0300

    [fix] Requisito 03

---
+++ src/pages/Header.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Components/Loading';
+
+export default class Header extends Component {
+  state = {
+    user: '',
+    inLoading: false,
+  };
+
+  componentDidMount() {
+    this.getStateUser();
+  }
+
+  getStateUser = async () => {
+    this.setState({ inLoading: true });
+    const request = await getUser();
+    this.setState({
+      user: request.name,
+      inLoading: false,
+    });
+  };
+
+  render() {
+    const { user, inLoading } = this.state;
+    return (
+      <div data-testid="header-component">
+        {
+          inLoading && <Loading />
+        }
+        <h3 data-testid="header-user-name">{ user }</h3>
+      </div>
+    );
+  }
+}
