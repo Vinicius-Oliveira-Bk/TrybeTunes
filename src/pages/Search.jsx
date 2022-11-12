f4b3f6439cd08fc39d106862093ffc6597110720
Vinicius-Oliveira-Bk <vinisouza21@hotmail.com>
2022-11-11 21:29:46 -0300

    [fix] Requisito 05 - Form: Search artist

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,47 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  state = {
+    submitButton: '',
+  };
+
+  isButtonDisabled = () => {
+    const { submitButton } = this.state;
+    const number = 2;
+    const validate = submitButton.length >= number;
+
+    return !(validate);
+  };
+
+  onInputChange = (event) => {
+    const { name, type, checked } = event.target;
+    const value = type === 'checkbox' ? checked : event.target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { submitButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            submitButton={ submitButton }
+            data-testid="search-artist-input"
+            onChange={ this.onInputChange }
+            name="submitButton"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ this.isButtonDisabled() }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
